ee5b2dbe36e6fd4092e28ed249e4b0e9838b8b9a
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-07-26 12:30:55 -0700

    minor tweaks

--- button_page.py
+++ button_page.py
@@ -8,12 +8,12 @@ class ButtonPage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
-    # easy_button_00 = (By.ID, 'easy00')
+    # actions against elements in the Easy to Sync section of the page
     def click_easy_button_00(self):
         self.wait.until(EC.element_to_be_clickable((By.ID, 'easy00'))).click()
 
     def click_easy_button_01(self):
-       self.wait.until(EC.element_to_be_clickable((By.ID, 'easy01'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'easy01'))).click()
 
     def click_easy_button_02(self):
         self.wait.until(EC.element_to_be_clickable((By.ID, 'easy02'))).click()
@@ -23,3 +23,20 @@ class ButtonPage:
 
     def easy_button_message_text(self):
         return self.driver.find_element(By.ID, 'easybuttonmessage').text
+
+    # actions against elements in the Harder to Sync section of the page
+    def click_hard_button_00(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'button00'))).click()
+
+    def click_hard_button_01(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'button01'))).click()
+
+    def click_hard_button_02(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'button02'))).click()
+
+    def click_hard_button_03(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'button03'))).click()
+
+    def hard_button_message_text(self):
+        return self.driver.find_element(By.ID, 'buttonmessage').text
+
--- test_button_page.py
+++ test_button_page.py
@@ -7,18 +7,29 @@ from button_page import ButtonPage
 @pytest.fixture(scope='class')
 def driver():
     driver = webdriver.Chrome()
+    driver.get('https://eviltester.github.io/synchole/buttons.html')
     yield driver
     driver.close()
 
 
-def test_easy_buttons(driver):
-    driver.get('https://eviltester.github.io/synchole/buttons.html')
+@pytest.fixture(scope='class')
+def button_page(driver):
+    return ButtonPage(driver)
 
-    button_page = ButtonPage(driver)
 
+def test_easy_buttons(driver, button_page):
     button_page.click_easy_button_00()
     button_page.click_easy_button_01()
     button_page.click_easy_button_02()
     button_page.click_easy_button_03()
 
     assert button_page.easy_button_message_text() == 'All Buttons Clicked'
+
+
+def test_hard_buttons(driver, button_page):
+    button_page.click_hard_button_00()
+    button_page.click_hard_button_01()
+    button_page.click_hard_button_02()
+    button_page.click_hard_button_03()
+
+    assert button_page.hard_button_message_text() == 'All Buttons Clicked'

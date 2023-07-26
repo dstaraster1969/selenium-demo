bcb42cdc24f72a854f38d9607758cb364c26d4fe
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-07-26 11:55:16 -0700

    elements on button_page

--- button_page.py
+++ button_page.py
@@ -1,8 +1,24 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class ButtonPage:
-    easy_button_00 = (By.ID, 'easy00')
-    easy_button_01 = (By.ID, 'easy01')
-    easy_button_02 = (By.ID, 'easy02')
-    easy_button_03 = (By.ID, 'easy03')
+    def __init__(self, wait):
+        self.wait = wait
+
+    # easy_button_00 = (By.ID, 'easy00')
+    def click_easy_button_00(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'easy00'))).click()
+
+    def click_easy_button_01(self):
+       self.wait.until(EC.element_to_be_clickable((By.ID, 'easy01'))).click()
+
+    def click_easy_button_02(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'easy02'))).click()
+
+    def click_easy_button_03(self):
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'easy03'))).click()
+
+    easy_button_message = (By.ID, 'easybuttonmessage')

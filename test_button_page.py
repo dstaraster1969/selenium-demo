67fd48c1e49d7017100a2804ee22ce18f020bf2a
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-07-26 12:06:36 -0700

    clean up

--- test_button_page.py
+++ test_button_page.py
@@ -1,8 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
 from button_page import ButtonPage
 
@@ -19,13 +16,9 @@ def test_easy_buttons(driver):
 
     button_page = ButtonPage(driver)
 
-    #wait.until(EC.element_to_be_clickable(button_page.easy_button_00)).click()
     button_page.click_easy_button_00()
     button_page.click_easy_button_01()
     button_page.click_easy_button_02()
     button_page.click_easy_button_03()
-    # wait.until(EC.element_to_be_clickable(buon_page.easy_button_01)).click()
-    # wait.until(EC.element_to_be_clickable(button_page.easy_button_02)).click()
-    # wait.until(EC.element_to_be_clickable(button_page.easy_button_03)).click()
 
     assert button_page.easy_button_message_text() == 'All Buttons Clicked'
